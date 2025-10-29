1c301668a16ed3bf9bf8d71a9502b758c52adfb5
Muhammad Aabiezar Farel Al-Fathir <2511500053@mahasiswa.atmaluhur.ac.id>
2025-10-29 20:08:54 +0700

    Penjelasan

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,9 +1,9 @@
-document.getElementById("menutoggle").addEventListener("click", function () {
+document.getElementById("menutoggle").addEventListener("click", function () { // Tombol hamburger, toggle navigasi menu ketika menu di click
     const nav = document.querySelector("nav");
-    nav.classList.toggle("active");
+    nav.classList.toggle("active"); // Menambahkan atau menghapus class "active" pada elemen <nav>
     if (nav.classList.contains("active")) {
-        this.textContent = "\u2716";
+        this.textContent = "\u2716"; // Mengubah teks tombol menjadi simbol "X" ketika menu aktif
     } else {
-        this.textContent = "\u2630";
+        this.textContent = "\u2630"; // Mengubah teks tombol kembali menjadi simbol hamburger ketika menu tidak aktif
     }
 });
